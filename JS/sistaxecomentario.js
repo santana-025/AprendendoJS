951f1a8c41838aa114334772e4d8f3142a815cd4
Jorge Santana <166669976+santana-025@users.noreply.github.com>
2024-04-28 18:56:32 -0300

    ad
    sdas

--- JS/sistaxecomentario.js
+++ JS/sistaxecomentario.js
@@ -1,6 +1,11 @@
-let = a, b, c;
+let a, b, c;
 a = 1;
 b = 5;
+c = 12;
+alert(a + b);
 console.log(a + b);
 console.log(1 + 5);
 console.log("1+5");
+//Comentario Simples
+/* Comentario com mais linhas 
+possibilitando textos e muito mas conteudos */
---
+++ JS/variavel.js
@@ -0,0 +1,10 @@
+var a = 12;
+var b = 15;
+var c = 10;
+//alert(a + b + c);
+// Melhor forma de fazer 
+var a, b, c;
+a = 3
+b = 9
+c = 5
+alert(a + c + b);
