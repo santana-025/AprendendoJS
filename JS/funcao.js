1b54258aa8c913ce972f8f1a7054b41801e94d00
Jorge Santana <166669976+santana-025@users.noreply.github.com>
2024-04-29 20:53:46 -0300

    vams

--- JS/funcao.js
+++ JS/funcao.js
@@ -2,10 +2,16 @@ function somar(x, y) {
     return x + y;
 }
 
-alert(somar(5, 8));
+//alert(somar(5, 8));
 
 function saudacao(nome) {
     console.log(`Olá ${nome}.`);
 }
 
-saudacao("jorge");
+//saudacao("jorge");
+
+function paraCelsius(valorf) {
+    return (5 / 9) * (valorf - 32);
+}
+
+alert(paraCelsius(70));
