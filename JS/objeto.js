85d3514eee45a8e097e5fd665bd01d109885532a
Jorge Santana <166669976+santana-025@users.noreply.github.com>
2024-04-29 21:15:45 -0300

    function em Objetos

--- JS/objeto.js
+++ JS/objeto.js
@@ -1,4 +1,14 @@
-/*let meuobjeto = { nome: "jorge", idade: 23, ano: 1998 };
-alert(meuobjeto.idade);*/
-let carro = { marca: "gol", modelo: "1.0", ano: 2020 };
-alert(carro.modelo);
+let meuobjeto = { nome: "jorge", idade: 23, ano: 1998 };
+//alert(meuobjeto.idade);
+let carro2 = { marca: "gol", modelo: "1.0", ano: 2020 };
+//alert(carro.modelo);
+
+let carro = {
+    buzina: function () { alert("buzinando") },
+    ligar: function () { alert("Ligando") },
+    andar: function () { alert("andando") }
+}
+
+alert(carro.buzina());
+alert(carro.ligar());
+alert(carro.andar());
