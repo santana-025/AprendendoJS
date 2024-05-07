019fc1174a5e45a40c4a7fd1e09ac0e7db1f2f6d
Jorge Santana <166669976+santana-025@users.noreply.github.com>
2024-05-06 22:37:08 -0300

    getElement

---
+++ JS/DOM/GetElement.js
@@ -0,0 +1,5 @@
+console.log(document.getElementById("texto").innerText);
+console.log(document.getElementsByName("nome_usuario"));
+document.getElementsByName("nome_usuario")[0].value = "João";
+console.log(document.getElementsByTagName("P"));
+console.log(document.getElementsByClassName("cor"));
