5122d8c95844907db2813bad98f659c03fb7053a
Jorge Santana <166669976+santana-025@users.noreply.github.com>
2024-05-11 23:19:08 -0300

    CursoEmVIDEO

---
+++ JS CursoEmVideos/ex013.js
@@ -0,0 +1,28 @@
+var hoje = new Date();
+var diasemana = hoje.getDay();
+switch (diasemana) {
+    case 0:
+        console.log("Domingo")
+        break
+    case 1:
+        console.log("Segunda")
+        break
+    case 2:
+        console.log("Terça")
+        break
+    case 3:
+        console.log("Quarta")
+        break
+    case 4:
+        console.log("Quinta")
+        break
+    case 5:
+        console.log("Sexta")
+        break
+    case 6:
+        console.log("Sabado")
+        break
+    default:
+        console.log("ERRO, Dia invalido..")
+        break
+}
