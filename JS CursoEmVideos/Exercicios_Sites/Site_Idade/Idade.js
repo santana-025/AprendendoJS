51cff302df4ed86abdf9bb4ed1b81f2755cb7f72
Jorge Santana <166669976+santana-025@users.noreply.github.com>
2024-05-12 12:02:57 -0300

    Ola

--- JS CursoEmVideos/Exercicios_Sites/Site_Idade/Idade.js
+++ JS CursoEmVideos/Exercicios_Sites/Site_Idade/Idade.js
@@ -3,11 +3,48 @@ function apertou() {
     let ano = data.getFullYear();
     let form_ano = document.getElementById("IDA");
     let res = document.querySelector("div#resu")
-    let sexo = document.querySelector()
-    if (form_ano.Value.length == 0 || form_ano > ano) {
-        window.alert("ERRO. Verifique os Dados e Tente Novamente!")
-    } else {
-        alert("Tudo Certo!")
-    }
 
+    if (form_ano.value.length == 0 || Number(form_ano.value) > ano) {
+        window.alert("Erro ! Verifique os dados e tente novamente !")
+    } else {
+        var genero = "";
+        var dados = document.getElementsByName('sexo');
+        var idade = ano - Number(form_ano.value);
+        var img = document.createElement("img");
+        img.setAttribute("id", "foto");
+        if (dados[0].checked) {
+            var genero = "Homem"
+            if (idade >= 0 && idade < 10) {
+                //Criança
+                img.setAttribute("src", "criança-m.jpg");
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute("src", "joven-m.jpg");
+            } else if (idade < 59) {
+                //Adulto
+                img.setAttribute("src", "adulto-m.jpg");
+            } else {
+                // Idoso
+                img.setAttribute("src", "idoso-m.jpg");
+            }
+        } else {
+            var genero = "Mulher"
+            if (idade >= 0 && idade < 10) {
+                //Criança
+                img.setAttribute("src", "criança-f.jpg");
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute("src", "joven-f.jpg");
+            } else if (idade < 59) {
+                //Adulto
+                img.setAttribute("src", "adulta.f.jpg");
+            } else {
+                // Idoso
+                img.setAttribute("src", "idosa-f.jpg");
+            }
+        }
+        res.style.textAlign = "center"
+        res.innerHTML = `Detectamos ${genero} com ${idade} Anos.<br>`
+        res.appendChild(img);
+    }
 }
