71d4f24502b51b00cbf0b3fdf09d1f5ca0d9ee71
Jorge Santana <166669976+santana-025@users.noreply.github.com>
2024-05-12 14:18:11 -0300

    qwe

---
+++ JS CursoEmVideos/node_e4735c6762a25.tmp.js
@@ -0,0 +1,13 @@
+
+// Fazendo manualmente
+
+/*console.log("tudo bem")
+console.log("tudo bem")
+console.log("tudo bem")
+*/
+
+//fazendo com while
+let c = 1;
+while (c <= 6) {
+    console.log("tudo bem")
+}
