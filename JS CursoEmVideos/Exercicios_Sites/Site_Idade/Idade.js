1c7b6a86a2407be60c01ef022a0d7ce2bd084096
Jorge Santana <166669976+santana-025@users.noreply.github.com>
2024-05-12 10:28:19 -0300

    msg

--- JS CursoEmVideos/Exercicios_Sites/Site_Idade/Idade.js
+++ JS CursoEmVideos/Exercicios_Sites/Site_Idade/Idade.js
@@ -1,1 +1,13 @@
-alert("oi")
+function apertou() {
+    let data = new Date();
+    let ano = data.getFullYear();
+    let form_ano = document.getElementById("IDA");
+    let res = document.querySelector("div#resu")
+    let sexo = document.querySelector()
+    if (form_ano.Value.length == 0 || form_ano > ano) {
+        window.alert("ERRO. Verifique os Dados e Tente Novamente!")
+    } else {
+        alert("Tudo Certo!")
+    }
+
+}
