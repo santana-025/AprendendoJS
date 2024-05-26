3ffb7e2ef3d5348a801a063b2d1d5d61b35c00ff
Jorge Santana <166669976+santana-025@users.noreply.github.com>
2024-05-26 14:10:56 -0300

    javs

--- JS Dicas Para Devs/20 Eventos.js
+++ JS Dicas Para Devs/20 Eventos.js
@@ -10,13 +10,24 @@ Tirar o modo defalt do btn, passa o proprio evento na função */
 const btn = document.querySelector("#mandar");
 const inptnome = document.querySelector("#nome");
 const inptemail = document.querySelector("#email");
+const meuformulario = document.querySelector("#formulario")
+const itens = document.querySelector(".kill")
 
 btn.addEventListener("click", function (e) {
     e.preventDefault();
     // Pegando os valores
-    const valornome = inptnome.value;       // .Value so funciona para queryselector
-    const valoremail = inptemail.value;
-    console.log("Oi");
-    console.log(valoremail);
+    let valornome = inptnome.value;       // .Value so funciona para queryselector
+    let valoremail = inptemail.value;
+    console.log(valornome, valoremail);
+    if (valornome === "" || valoremail === "") {
+        return alert("Preencha todos os campos do Formulario") // Return para sair da função
+    }
+    meuformulario.style.background = "Red"  // Mudando a cor do form
+    itens.firstElementChild.innerHTML = `Nome: ${valornome}`
+    itens.children[1].innerHTML = ` Email: ${valoremail}`
+
 })
 
+inptnome.addEventListener("change", function (e) {  // Passando o imput para receber o evento change que é mudança
+    //console.log(e.target.value)         // todas as vezes que mudar o conteudo ele vai da um console.log da mudança
+})
